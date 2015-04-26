e85c932fb9e4ff485e71d52539bdaf908c3279f1
yale8809 <guoyu8809@sina.com>
2015-04-26 23:03:46 +0800

    c++ exame

--- CPlusPlus/Polymorphism/Polymorphism.cpp
+++ CPlusPlus/Polymorphism/Polymorphism.cpp
@@ -76,6 +76,9 @@ public:
     double Bvalue(double x){return Avalue(x)/2;};
     double Cvalue(double x){return x*x;};
     double Dvaule(double x){return Cvalue(x)/2;};
+
+    void print(){cout<<"print"<<endl;};
+    void print() const {cout<<"const print"<<endl;};
 };
 
 class B: public A
@@ -89,6 +92,70 @@ struct E{virtual ~E(){};};
 struct D{virtual ~D(){};};
 struct C:public D,public E{virtual ~C(){};};
 
+class Father
+{
+protected:
+    int f1;
+    int f2;
+public:
+    Father():f1(0),f2(0){cout<<"Father ctor"<<endl;};
+    Father(const Father& f):f1(f.f1), f2(f.f2){cout<<"Father copy ctor"<<endl;};
+    virtual ~Father(){cout<<"Father dtor"<<endl;};
+
+};
+
+class Son: public Father
+{
+protected:
+    int s1;
+    int s2;
+    Father f;
+public:
+    Son():s1(0),s2(0){cout<<"Son ctor"<<endl;};
+    Son(const Son& s):s1(s.s1),s2(s.s2){cout<<"Son copy ctor"<<endl;};
+    virtual ~Son(){cout<<"Son dtor"<<endl;};
+
+};
+
+class Father1
+{
+public:
+    Father1(){cout<<"Father1 ctor"<<endl;};
+    virtual ~Father1(){cout<<"Father1 dtor"<<endl;};
+
+};
+
+class Son1: public Father1
+{
+    Father1 f;
+public:
+    Son1(){cout<<"Son1 ctor"<<endl;};
+    virtual ~Son1(){cout<<"Son1 dtor"<<endl;};
+
+};
+
+class Con
+{
+    const int c;
+public:
+    Con():c(1){};
+};
+
+class New
+{
+    static int x;
+    int y;
+public:
+    static void* operator new(size_t size)
+    {
+        x = 0;
+        //y = 0;
+        return malloc(size);
+    };
+};
+
+int New::x = 0;
+
 int _tmain(int argc, _TCHAR* argv[])
 {
     fun1();
@@ -100,6 +167,27 @@ int _tmain(int argc, _TCHAR* argv[])
     C *c = new C();
     E *e = (E*)c;
     D *d = (D*)c;
+    delete c;
+
+    Father* f = new Son();
+    Son s = *(Son *)f;
+    delete f;
+
+    Son1 s1;
+    Father1& f2 = s1;
+    cout<<"-------------"<<endl;
+    Father1* f1 = new Son1();
+    delete f1;
+    cout<<"-------------"<<endl;
+
+
+    A a1;
+    const A a2;
+    a1.print();
+    a2.print();
+
+    New* n = new New();
+    delete n;
 	return 0;
 }
 
